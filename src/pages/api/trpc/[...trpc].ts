d6d21f5a8294f95e39cf0618da133a81c481bf08
Taj K <taj.kirkpatrick@gmail.com>
2023-01-15 17:33:57 -0600

    feat(core): eslint && client typesafety

--- src/pages/api/trpc/[...trpc].ts
+++ src/pages/api/trpc/[...trpc].ts
@@ -1,7 +1,7 @@
-import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import type { APIRoute } from "astro";
-import { createContext } from "../../server/trpc/context";
-import { appRouter } from "../../server/trpc/router/_app";
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { appRouter } from "../../../server/trpc/router/_app";
+import { createContext } from "../../../server/trpc/context";
 
 export const all: APIRoute = ({ request }) => {
   return fetchRequestHandler({
